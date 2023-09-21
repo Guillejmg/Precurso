da362cb06e2d61da4470e37c1d4aff1fdad5b73a
Guille <g.mirandagaleote@gmail.com>
2023-09-21 11:57:02 +0200

    update add exercises

---
+++ ejercicio12.js
@@ -0,0 +1,7 @@
+//Crea una función que reciba un número como parámetro e imprima por consola si el número dado es par o impar.
+//Nota: buscar como se clona un repo en github
+const kindOfNumber = (X) => {
+    if (x % 2 != 0) {
+        console.log ('este numero es impar')
+    } 
+}
